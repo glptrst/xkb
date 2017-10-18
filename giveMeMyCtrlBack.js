a0bcc6b46477b4b58ffa5b81f18786152859e240
Giulio Pietroiusti <giulio.pietroiusti@gmail.com>
2017-10-18 14:37:59 +0200

    Provide different options

--- giveMeMyCtrlBack.js
+++ giveMeMyCtrlBack.js
@@ -1,8 +1,19 @@
 var fs = require('fs');
 
-//TODO: give the possiblity of using different evdev files by giving different arguments
+var arg = process.argv[2];
 
-fs.copyFile('./evdev_for_MACBOOKPRO_2/evdev', './/usr/share/X11/xkb/keycodes/evdev', (err) => {
-    if (err) throw err;
-    console.log('source.txt was copied to destination.txt');
-});
+if (arg === '1') {
+    fs.copyFile('./evdev_for_MACBOOKPRO_1/evdev', '/usr/share/X11/xkb/keycodes/evdev', (err) => {
+	if (err) throw err;
+	console.log('Keyboard successfully modified');
+    });
+} else if (arg === '2') {
+    fs.copyFile('./evdev_for_MACBOOKPRO_2/evdev', '/usr/share/X11/xkb/keycodes/evdev', (err) => {
+	if (err) throw err;
+	console.log('Keyboard successfully modified');
+    });
+} else if (arg === undefined) {
+    console.log('Provide an argument');
+} else {
+    console.log('Unknown argument');
+}
